17bd4fcd3c935edefcc27c4a15a31a7f98d5ffc5
meghanblyth <megblyth@gmail.com>
2022-03-07 16:49:41 +0000

    Step 5, implementing the notes model class

---
+++ notesModel.js
@@ -0,0 +1,22 @@
+class Note {
+
+  constructor() {
+    this.notes = [];
+  }
+
+  getNotes() {
+    return this.notes;
+  }
+
+  addNote(note) {
+    this.notes.push(note);
+  }
+
+  reset() {
+    this.notes = [];
+  }
+
+}
+
+module.exports = Note;
+let notes = new Note;
---
+++ notesModel.test.js
@@ -0,0 +1,32 @@
+const Note = require('./notesModel');
+
+describe('notes', () => {
+  
+  let notes;
+
+  beforeEach(() => {
+    notes = new Note();
+  });
+
+  it('starts with no notes', () => {
+    expect(notes.getNotes()).toEqual([]);
+  })
+
+  it('can add a note', () => {
+  
+    notes.addNote('Buy milk');
+    expect(notes.getNotes()).toEqual(['Buy milk']);
+  })
+
+  it('resets the list of notes', () => {
+    notes.addNote('Buy milk');
+    notes.reset();
+
+    expect(notes.getNotes()).toEqual([]);
+  })
+
+})
+
+
+
+
